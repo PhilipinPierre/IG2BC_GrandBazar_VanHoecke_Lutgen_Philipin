a9d801309529a07b6a49e9ec078500e1893f4dfe
PhilipinPierre <39002218+PhilipinPierre@users.noreply.github.com>
2018-05-14 19:31:55 +0200

    13-05 MAJ DA

--- src/dataAccessPackage/SingletonConnexion.java
+++ src/dataAccessPackage/SingletonConnexion.java
@@ -1,5 +1,7 @@
 package dataAccessPackage;
 
+import exceptionsPackage.ExceptionsBD;
+
 import java.sql.*;
 import javax.naming.*;
 import javax.sql.DataSource;
@@ -7,11 +9,19 @@ import javax.sql.DataSource;
 public class SingletonConnexion {
     private static Connection instance;
 
-    public static Connection getInstance() throws SQLException, NamingException{
-        if(instance == null){
-            Context context = new InitialContext();
-            DataSource source = (DataSource)context.lookup("jdbc:mysql://localhost:3306/dbmagasin");
-            instance = source.getConnection();
+    public static Connection getInstance() throws ExceptionsBD{
+        try{
+            if(instance == null){
+                System.out.println("Ex");
+                Context context = new InitialContext();
+                System.out.println("Contexte");
+                DataSource source = (DataSource)context.lookup("jdbc:mysql://localhost:3306/dbmagasin");
+                System.out.println("Source");
+                instance = source.getConnection();
+                System.out.println("Instance");
+            }
+        } catch (Exception e){
+            throw new ExceptionsBD(" connexion a la base de données");
         }
         return instance;
     }
