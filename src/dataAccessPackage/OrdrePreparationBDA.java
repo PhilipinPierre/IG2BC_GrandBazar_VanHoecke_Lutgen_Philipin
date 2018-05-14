0cbd8c9044affbe045440f22ac3b913b135d6823
PhilipinPierre <39002218+PhilipinPierre@users.noreply.github.com>
2018-05-14 18:34:32 +0200

    13-05 MAJ DA

--- src/dataAccessPackage/OrdrePreparationBDA.java
+++ src/dataAccessPackage/OrdrePreparationBDA.java
@@ -2,12 +2,9 @@ package dataAccessPackage;
 
 import exceptionsPackage.ExceptionsBD;
 import modelPackage.OrdrePreparation;
+import modelPackage.TypeArticle;
 
-import javax.naming.NamingException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
 
@@ -27,6 +24,48 @@ public class OrdrePreparationBDA implements OrdrePreparationDA {
             return liste;
         } catch (Exception e){
             throw  new ExceptionsBD("recherche de tout les ordres de préparations");
+        }
+    }
+
+    public void SetOrdrePreparation(OrdrePreparation ordrePreparation) throws ExceptionsBD{
+        try{
+            Connection connection = SingletonConnexion.getInstance();
+            String requeteSQL = "insert into ordrepreparation values(?,?,?,?,?,?,?,?,?,?,?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(requeteSQL);
+            preparedStatement.setDate(1, new java.sql.Date(ordrePreparation.getDate().getTimeInMillis()));
+            preparedStatement.setInt(2,ordrePreparation.getNumeroSequentiel());
+            preparedStatement.setInt(3,ordrePreparation.getQuantitePrevue());
+            if(ordrePreparation.getQuantiteProduite() == null)
+                preparedStatement.setNull(4, Types.INTEGER);
+            else
+                preparedStatement.setInt(4, ordrePreparation.getQuantiteProduite());
+            if(ordrePreparation.getDateVente() == null)
+                preparedStatement.setNull(5, Types.TIMESTAMP);
+            else
+                preparedStatement.setDate(5, new java.sql.Date(ordrePreparation.getDateVente().getTimeInMillis()));
+            if(ordrePreparation.getDatePreparation() == null)
+                preparedStatement.setNull(6, Types.TIMESTAMP);
+            else
+                preparedStatement.setDate(6, new java.sql.Date(ordrePreparation.getDatePreparation().getTimeInMillis()));
+            if(ordrePreparation.getRemarque() == null)
+                preparedStatement.setNull(7, Types.VARCHAR);
+            else
+                preparedStatement.setString(7, ordrePreparation.getRemarque());
+            preparedStatement.setBoolean(8, ordrePreparation.getEstUrgent());
+            preparedStatement.setString(9, ordrePreparation.getNom().getNom());
+            if(ordrePreparation.getCodeBarre().getCodeBarre() == null)
+                preparedStatement.setNull(10, Types.INTEGER);
+            else
+                preparedStatement.setInt(10, ordrePreparation.getCodeBarre().getCodeBarre());
+            if(ordrePreparation.getMatriculeCui().getMatricule() == null)
+                preparedStatement.setNull(11, Types.INTEGER);
+            else
+                preparedStatement.setInt(11, ordrePreparation.getMatriculeCui().getMatricule());
+            preparedStatement.setInt(12, ordrePreparation.getMatriculeRes().getMatricule());
+
+
+        } catch (Exception e){
+            throw  new ExceptionsBD(" accès à la base de données");
         }
     }
 
