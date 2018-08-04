6aac34538126ed75b2773866726c931da8520fd6
etu32766 <etu32766@henallux.be>
2018-08-04 18:07:18 +0200

    Ajout de la tache metier

--- src/viewPackage/FournisseurInsertion.java
+++ src/viewPackage/FournisseurInsertion.java
@@ -3,12 +3,130 @@ package viewPackage;
 import controllerPackage.ApplicationController;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FournisseurInsertion extends JPanel {
+    private JPanel panneauInsertion, panneauBoutons;
     private ApplicationController applicationController;
+    private JLabel numeroTvaLabel, nomLabel, localiteLabel, codePostalLabel, rueLabel;
+    private JTextField numeroTva, nom, localite, codePostal, rue;
+    private JButton retour, ajoutFournisseur, reinitialiser;
+
 
     public FournisseurInsertion(ApplicationController applicationController)
     {
         this.applicationController = applicationController;
+
+        setLayout(new BorderLayout());
+
+        //FORMULAIRE
+        panneauInsertion = new JPanel();
+
+        panneauInsertion.setLayout(new GridLayout(6, 2, 5, 5));
+
+        //NUMERO DE TVA
+        numeroTvaLabel = new JLabel("Numéro de TVA : ");
+        numeroTvaLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        panneauInsertion.add(numeroTvaLabel);
+        numeroTva = new JTextField();
+        panneauInsertion.add(numeroTva);
+
+        //NOM
+        nomLabel = new JLabel("Nom : ");
+        nomLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        panneauInsertion.add(nomLabel);
+        nom = new JTextField();
+        panneauInsertion.add(nom);
+
+        //LOCALITE
+        localiteLabel = new JLabel("Localité : ");
+        localiteLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        panneauInsertion.add(localiteLabel);
+        localite = new JTextField();
+        panneauInsertion.add(localite);
+
+        //CODE POSTAL
+        codePostalLabel = new JLabel("Code postal : ");
+        codePostalLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        panneauInsertion.add(codePostalLabel);
+        codePostal = new JTextField();
+        panneauInsertion.add(codePostal);
+
+        //RUE
+        rueLabel = new JLabel("Rue : ");
+        rueLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        panneauInsertion.add(rueLabel);
+        rue = new JTextField();
+        panneauInsertion.add(rue);
+
+
+        //BOUTONS
+        panneauBoutons = new JPanel();
+
+        panneauBoutons.setLayout(new FlowLayout());
+
+        retour = new JButton("Retour");
+        panneauBoutons.add(retour);
+        FournisseurInsertion.ButtonListenerRetour listenerRetour = new FournisseurInsertion.ButtonListenerRetour();
+        retour.addActionListener(listenerRetour);
+        ajoutFournisseur = new JButton("Ajouter fournisseur");
+        panneauBoutons.add(ajoutFournisseur);
+        FournisseurInsertion.ButtonListenerAjouterFournisseur listenerAjouterFournisseur = new FournisseurInsertion.ButtonListenerAjouterFournisseur();
+        ajoutFournisseur.addActionListener(listenerAjouterFournisseur);
+        reinitialiser = new JButton("Réinitialiser");
+        panneauBoutons.add(reinitialiser);
+        FournisseurInsertion.ButtonListenerReinitialiser listenerReinitialiser = new FournisseurInsertion.ButtonListenerReinitialiser();
+        reinitialiser.addActionListener(listenerReinitialiser);
+
+        add(panneauInsertion, BorderLayout.NORTH);
+
+        add(panneauBoutons, BorderLayout.SOUTH);
+
+        setEnabled(true);
     }
+
+    //CLASSES PRIVEES POUR LES BOUTONS
+    private class ButtonListenerRetour implements ActionListener
+    {
+        public void actionPerformed(ActionEvent event)
+        {
+            removeAll();
+            validate();
+
+            add(new MessageAccueil(), BorderLayout.CENTER);
+
+            revalidate();
+            repaint();
+        }
+    }
+
+    private class ButtonListenerAjouterFournisseur implements ActionListener
+    {
+        public void actionPerformed(ActionEvent event)
+        {
+            try
+            {
+                ;
+            }
+            catch (Exception e)
+            {
+                JOptionPane.showMessageDialog(panneauBoutons, e.getMessage(), "Erreur lors de l'ajout d'une nouvelle recette", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    private class ButtonListenerReinitialiser implements ActionListener
+    {
+        public void actionPerformed(ActionEvent event)
+        {
+            numeroTva.setText(null);
+            nom.setText(null);
+            localite.setText(null);
+            codePostal.setText(null);
+            rue.setText(null);
+        }
+    }
+
 }
