609e0549bacaf7393bd08934cedb227c8fd7b4b5
etu32766 <etu32766@henallux.be>
2018-08-07 21:58:41 +0200

    recetteinsertion gestion des erreurs

--- src/viewPackage/PanelRecette.java
+++ src/viewPackage/PanelRecette.java
@@ -2,17 +2,20 @@ package viewPackage;
 
 import controllerPackage.ApplicationController;
 import modelPackage.Recette;
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class PanelRecette extends JPanel {
+    private JPanel panneauBoutons;
     private JTable table;
     private JLabel titre;
     private ListSelectionModel listSelect;
     private ApplicationController applicationController;
     private ModeleRecette modeleRecette;
+    private JButton calculDlc;
 
     public PanelRecette(ApplicationController applicationController, ArrayList<Recette> recette)
     {
@@ -27,6 +30,26 @@ public class PanelRecette extends JPanel {
         table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
         JScrollPane defilant = new JScrollPane(table);
         add(defilant, BorderLayout.CENTER);
+
+        panneauBoutons = new JPanel();
+
+        panneauBoutons.setLayout(new FlowLayout());
+
+        calculDlc = new JButton("Nombre total de dlc");
+        panneauBoutons.add(calculDlc);
+        PanelRecette.ButtonListenerCalculDlc listenerCalculDlc = new PanelRecette.ButtonListenerCalculDlc();
+        calculDlc.addActionListener(listenerCalculDlc);
+
+        add(panneauBoutons, BorderLayout.SOUTH);
+
         setVisible(true);
+    }
+
+    private class ButtonListenerCalculDlc implements ActionListener
+    {
+        public void actionPerformed(ActionEvent event)
+        {
+
+        }
     }
 }
